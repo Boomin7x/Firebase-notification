00145006f81c20cde8e8cc5a1d925497f3fd35bc
boomin7x <kodiahbetro@gmail.com>
2025-06-03 02:15:20 +0100

    Update Firebase configuration: replace existing credentials with new project details

--- app/(routes)/v2/_utils/firebase/initialise.tsx
+++ app/(routes)/v2/_utils/firebase/initialise.tsx
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAJGIgUzhTqWD0i4GTfOYaKXf6kn45RFmM",
-  authDomain: "notifications-fd96d.firebaseapp.com",
-  projectId: "notifications-fd96d",
-  storageBucket: "notifications-fd96d.firebasestorage.app",
-  messagingSenderId: "150858804472",
-  appId: "1:150858804472:web:656568e02b81d759015f0d",
+  apiKey: "AIzaSyD03fCwB4EoR_rN25cl7P8vw2k_bRh8Ca4",
+  authDomain: "maliya-4b31c.firebaseapp.com",
+  projectId: "maliya-4b31c",
+  storageBucket: "maliya-4b31c.firebasestorage.app",
+  messagingSenderId: "294257136832",
+  appId: "1:294257136832:web:d3250dd77f21dd6e294925",
+  measurementId: "G-DWCBD1JDR3",
 };
 
 export const fireBaseInitialized = initializeApp(firebaseConfig);
